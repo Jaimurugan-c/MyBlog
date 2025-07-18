0713172926b54b9439685f42504b4eed4dac1037
Jaimurugan-c <jaimurugan1703@gmail.com>
2025-07-18 16:51:27 +0530

    Mongoose

--- backend/index.js
+++ backend/index.js
@@ -1,12 +1,27 @@
 import express from 'express';
-
+import mongoose from 'mongoose';
 const app = express();
-const PORT = process.env.PORT || 4000;
 
-app.get('/page', (req,res)=>{
-    res.send("Hello")
-})
 
-app.listen(PORT,()=>{
-    console.log(`Server is running on port ${PORT}`)
-})
+ app.use(express.json());
+ const PORT = process.env.PORT || 3000;
+
+ app.get('/',(req,res)=>{
+    res.send('Hllo!');
+ })
+
+
+
+ //Db
+ mongoose.connect("mongodb://localhost:27017/MineBlog",{
+     
+   }).then(() => {
+      console.log("Connected to MongoDB");
+   }).catch((err) => {
+      console.error("Error connecting to MongoDB:", err);
+ })
+ 
+
+ app.listen (PORT,()=>{
+    console.log(`Server is running on port ${PORT}`);
+ })
