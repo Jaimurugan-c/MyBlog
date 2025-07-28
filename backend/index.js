7c0a526c6f07d4303e60027d22a87678ea14d218
Jaimurugan-c <jaimurugan1703@gmail.com>
2025-07-28 11:26:12 +0530

    20%

--- backend/index.js
+++ backend/index.js
@@ -1,25 +1,28 @@
 import express from 'express';
 import mongoose from 'mongoose';
+import userSchema from  './models/user.js';
+import dotenv from 'dotenv';
 const app = express();
-
+dotenv.config(); // env 
 
  app.use(express.json());
  const PORT = process.env.PORT || 3000;
 
+
+ //Api
  app.get('/',(req,res)=>{
     res.send('Hllo!');
  })
 
-
+ 
 
  //Db
- mongoose.connect("mongodb://localhost:27017/MineBlog",{
-     
-   }).then(() => {
-      console.log("Connected to MongoDB");
-   }).catch((err) => {
-      console.error("Error connecting to MongoDB:", err);
- })
+mongoose.connect(process.env.MONGO_URI, {
+
+}).then(() => console.log('MongoDB Connected'))
+.catch((err) => console.error('MongoDB connection error:', err));
+
+
  
 
  app.listen (PORT,()=>{
