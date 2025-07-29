8e7299bc6d5359114345008d48cab5df5cbb3e68
Jaimurugan-c <jaimurugan1703@gmail.com>
2025-07-29 16:43:11 +0530

    restart

--- backend/index.js
+++ backend/index.js
@@ -1,30 +1,13 @@
 import express from 'express';
-import mongoose from 'mongoose';
-import userSchema from  './models/user.js';
-import dotenv from 'dotenv';
+const PORT = 3000;
 const app = express();
-dotenv.config(); // env 
 
- app.use(express.json());
- const PORT = process.env.PORT || 3000;
-
-
- //Api
- app.get('/',(req,res)=>{
-    res.send('Hllo!');
- })
-
- 
-
- //Db
-mongoose.connect(process.env.MONGO_URI, {
-
-}).then(() => console.log('MongoDB Connected'))
-.catch((err) => console.error('MongoDB connection error:', err));
+app.get ('/', (req,res)=>{
+    res.send("Blog")
+})
 
 
- 
 
- app.listen (PORT,()=>{
-    console.log(`Server is running on port ${PORT}`);
- })
+app.listen(PORT,()=>{
+    console.log(`Server is running on http://localhost:${PORT}`); 
+})
