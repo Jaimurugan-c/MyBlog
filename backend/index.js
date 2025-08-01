6b5f1021d77bbc7483eae288f5234d7322181a51
Jaimurugan-c <jaimurugan1703@gmail.com>
2025-08-01 15:46:09 +0530

    kathirupom

--- backend/index.js
+++ backend/index.js
@@ -1,9 +1,21 @@
 import express from 'express';
+import mongoose from 'mongoose';
+import dotenv from 'dotenv';
+dotenv.config();
 const PORT = 3000;
 const app = express();
+const MONGO_URI = process.env.MONGO_URI;
 
 app.get ('/', (req,res)=>{
     res.send("Blog")
+})
+
+//mongodb connection mamee
+mongoose.connect(MONGO_URI,{
+}).then(() => {
+    console.log("MongoDB connected successfully");
+}).catch((err) => {
+    console.error("MongoDB connection error:", err);
 })
 
 
